6855fe1adb9ea5ce250def220a5264906dab02b2
gjakubik <gavito8800@gmail.com>
2021-10-04 20:48:13 -0400

    changed structure to feature based

---
+++ feature3/src/components/AboutView/index.jsx
@@ -0,0 +1,9 @@
+import './style';
+
+export default function AboutView() {
+	return (
+		<div>
+			<h1>About View</h1>
+		</div>
+	);
+}
---
+++ feature3/src/components/ContactView/index.jsx
@@ -0,0 +1,9 @@
+import './style';
+
+export default function ContactView() {
+	return (
+		<div>
+			<h1>Contact View</h1>
+		</div>
+	);
+}
