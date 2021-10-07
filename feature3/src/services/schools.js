9ed524807a6b07a801d7583db7aeb4aebcff0261
Quinn Tobey Bardwell <quinntobeybardwell@quinns-air-2.dhcp.nd.edu>
2021-10-06 22:09:52 -0400

    last commit

--- feature3/src/services/schools.js
+++ feature3/src/services/schools.js
@@ -2,5 +2,6 @@ import axios from "axios";
 import { SCHOOLS } from "../constants.js"
 
 export const getSchools = () => {
+    //Grabbing json data from constants.js file
     return SCHOOLS.results;
 };
