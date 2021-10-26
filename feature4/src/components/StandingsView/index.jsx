e855c5dfc76f9dfdad0ebfd03d72ff524a194e0a
gjakubik <gavito8800@gmail.com>
2021-10-26 10:58:03 -0400

    did some stuff, but making new project

--- feature4/src/components/StandingsView/index.jsx
+++ feature4/src/components/StandingsView/index.jsx
@@ -1,11 +1,11 @@
 import './style';
-import StandingsList from "../StandingList";
+import StandingsList from "./StandingList";
+
 
 export default function StandingsView() {
 	return (
 		<div>
-			<h1>Standings View</h1>
-			<StandingsList />
+			
 		</div>
 	);
 }
