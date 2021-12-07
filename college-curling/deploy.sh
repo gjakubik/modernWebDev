32bc2dc167b155ba80159cb6e4abc2239a646776
gjakubik <gavito8800@gmail.com>
2021-12-07 14:14:55 -0500

    trying stuff

--- college-curling/deploy.sh
+++ college-curling/deploy.sh
@@ -7,6 +7,8 @@ sudo npm ci
 echo "Generating site..."
 sudo npm run build
 
+echo "Removing old site..."
+sudo rm -rf /var/www/public
 #Copy to server root
 echo "Moving to server root.."
 sudo cp -r public /var/www/
